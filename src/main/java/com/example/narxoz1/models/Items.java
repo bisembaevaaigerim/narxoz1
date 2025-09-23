3b05df906cb8b1bc759edc608996f8f216451878
bisembaevaaigerim <bisembaeva.aigerim2006@gmail.com>
2025-09-23 13:14:47 +0500

    Items.java

--- src/main/java/com/example/narxoz1/models/Items.java
+++ src/main/java/com/example/narxoz1/models/Items.java
@@ -2,11 +2,9 @@ package com.example.narxoz1.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Items {
     Long id;
     String name;
