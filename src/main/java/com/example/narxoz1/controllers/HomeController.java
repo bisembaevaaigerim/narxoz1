b79d324ee2866624bba68265dd8b7b544c0aad8d
bisembaevaaigerim <bisembaeva.aigerim2006@gmail.com>
2025-09-30 23:58:32 +0500

    HomeController.java

--- src/main/java/com/example/narxoz1/controllers/HomeController.java
+++ src/main/java/com/example/narxoz1/controllers/HomeController.java
@@ -11,12 +11,13 @@ public class HomeController {
     @GetMapping("/")
     public String indexPage(Model model) {
         ArrayList<Items> books = new ArrayList<>();
-        books.add(new Items(1L, "Harry Potter and the philosopher's stone", "J.K. Rowling", 19.99));
-        books.add(new Items(2L, "Harry Potter and the chamber of secrets", "J.K. Rowling", 21.50));
+        
+        books.add(new Items(1L, "Harry Potter", "J.K. Rowling", 20.90));
+        books.add(new Items(2L, "Fight club", "Chuck Palahniuk", 21.50));
         books.add(new Items(3L, "The shining", "Stephen King", 18.75));
         books.add(new Items(4L, "It", "Stephen King", 22.99));
-        books.add(new Items(5L, "The lord of the rings", "J.R.R. Tolkien", 25.00));
-        books.add(new Items(6L, "A game of thrones", "George R.R. Martin", 24.50));
+        books.add(new Items(5L, "The lord of the rings", "J.R. Tolkien", 25.00));
+        books.add(new Items(6L, "A game of thrones", "George R. Martin", 24.50));
 
         model.addAttribute("items", books);
         return "index";
